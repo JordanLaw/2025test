3e4f7edc9dd77ce947d6d2df0a83aceb64d76449
JordanLaw <123150515+JordanLaw@users.noreply.github.com>
2025-02-05 10:55:38 +0800

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+cam =cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+while True:
+    ret, frame = cam.read()
+
+    frame = cv2.Canny(frame, 100, 300)
+
+    cv2.imshow('cam', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
